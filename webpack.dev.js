06cd6d917865cf882141ca1c05c30c07e8e1a5db
hleao <henrique.leao@softvaro.com.br>
2022-09-05 14:32:45 -0300

    chore: :sparkles: add lint to project

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,4 +1,3 @@
-const path = require('path');
 const { merge } = require('webpack-merge');
 const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -7,12 +6,12 @@ const common = require('./webpack');
 
 module.exports = merge(common, {
   mode: 'development',
-  devtool: 'eval-source-map', 
+  devtool: 'eval-source-map',
   devServer: {
     open: true,
     port: 3000,
     hot: true,
-    historyApiFallback: true,
+    historyApiFallback: true
   },
   plugins: [
     new HtmlWebpackPlugin({
@@ -21,9 +20,11 @@ module.exports = merge(common, {
     new FriendlyErrorsWebpackPlugin({
       compilationSuccessInfo: {
         messages: ['You application is running here http://localhost:3000'],
-        notes: ['Some additionnal notes to be displayed unpon successful compilation']
+        notes: [
+          'Some additionnal notes to be displayed unpon successful compilation'
+        ]
       },
-      clearConsole: true,
-    }),
+      clearConsole: true
+    })
   ]
 });

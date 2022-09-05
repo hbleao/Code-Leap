117a0d3d5aa8c8a83785515e08ae57ca25c0687c
hleao <henrique.leao@softvaro.com.br>
2022-09-05 14:04:59 -0300

    chore: :wrench: typescript configuration

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,17 +1,29 @@
 const path = require('path');
 const { merge } = require('webpack-merge');
+const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const common = require('./webpack');
 
 module.exports = merge(common, {
   mode: 'development',
   devtool: 'eval-source-map', 
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
+    open: true,
     port: 3000,
     hot: true,
     historyApiFallback: true,
-    overlay: {
-      warnings: true,
-      errors: true,
-    }
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './templates/template.dev.html'
+    }),
+    new FriendlyErrorsWebpackPlugin({
+      compilationSuccessInfo: {
+        messages: ['You application is running here http://localhost:3000'],
+        notes: ['Some additionnal notes to be displayed unpon successful compilation']
+      },
+      clearConsole: true,
+    }),
+  ]
 });
--- webpack.js
+++ webpack.js
@@ -1,19 +1,26 @@
 const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: path.join(__dirname, 'src', 'index.tsx'),
   output: {
-    path: path.join(__dirname, '..', '..', 'public', 'js'),
+    path: path.join(__dirname, 'public', 'js'),
     filename: 'bundle.js',
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', 'jsx'],
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
   },
   module: {
     rules: [
       {
         test: /\.ts(x?)$/,
         loader: 'ts-loader',
+        options: {
+          transpileOnly: true
+        }
       },
       {
         test: /\.(png|jpe?g|gif)$/i,
@@ -24,4 +31,7 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new CleanWebpackPlugin()
+  ]
 }

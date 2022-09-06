521bdbfb38ca8c7b1e439325b022568179398ad2
hleao <henrique.leao@softvaro.com.br>
2022-09-05 21:52:48 -0300

    chore: :books: added storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { ThemeProvider } from 'styled-components';
+
+import theme from 'styles/theme';
+
+import { GlobalStyles } from 'styles/global';
+
+export const decorators = [
+  Story => (
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Story />
+    </ThemeProvider>
+  )
+];

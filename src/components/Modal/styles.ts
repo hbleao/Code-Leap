dd66ca7af2f7203e2db52e01d7035078b475ab70
hleao <henrique.leao@softvaro.com.br>
2022-09-09 16:41:30 -0300

    refactor: :recycle: responsivity

--- src/components/Modal/styles.ts
+++ src/components/Modal/styles.ts
@@ -1,4 +1,5 @@
 import styled, { css } from 'styled-components';
+import media from 'styled-media-query';
 
 import { BoxContentProps, ContainerProps } from './types';
 
@@ -7,15 +8,33 @@ const modifiers = {
     display: none;
   `,
   small: () => css`
-    width: 50rem;
+    width: 90%;
+
+    ${media.greaterThan('small')`
+      width: 50rem;
+    `}
   `,
   medium: () => css`
-    width: 66rem;
+    width: 90%;
+
+    ${media.greaterThan('small')`
+        width: 66rem;
+    `}
+  `,
+  large: () => css`
+    width: 90%;
+
+    ${media.greaterThan('small')`
+        width: 72rem;
+    `}
   `
 };
 
 export const Container = styled.section<ContainerProps>`
   ${({ theme, isOpen }) => css`
+    position: fixed;
+    overflow: hidden;
+    top: 0;
     width: 100vw;
     height: 100vh;
     max-width: 100%;
@@ -33,6 +52,7 @@ export const Container = styled.section<ContainerProps>`
 
 export const BoxContent = styled.div<BoxContentProps>`
   ${({ theme, size }) => css`
+    width: 90%;
     background-color: ${theme.colors.white};
     padding: ${theme.spacings.medium};
     animation: moveUp 0.5s cubic-bezier(0.165, 0.84, 0.44, 1) forwards;
--- src/components/Modal/types.ts
+++ src/components/Modal/types.ts
@@ -3,7 +3,7 @@ import { ReactNode } from 'react';
 export type ModalProps = {
   children: ReactNode;
   isOpen: boolean;
-  size?: 'small' | 'medium';
+  size?: 'small' | 'medium' | 'large';
 };
 
 export type ContainerProps = Pick<ModalProps, 'isOpen'>;

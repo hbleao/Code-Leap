7103fc36747761948d5421d3a7b96be977c525ae
hleao <henrique.leao@softvaro.com.br>
2022-09-10 15:39:36 -0300

    feat: :sparkles: added redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
+import { Provider as ReduxProvider } from 'react-redux';
+import { QueryClientProvider, QueryClient } from 'react-query';
+
+import { GlobalStyles } from './styles/global';
 
 import { ApplicationRoutes } from './routes';
-import { GlobalStyles } from './styles/global';
+
 import theme from './styles/theme';
+import store from './redux/store';
+
+const queryClient = new QueryClient();
 
 export const App = () => {
   return (
-    <ThemeProvider theme={theme}>
-      <ApplicationRoutes />
-      <GlobalStyles />
-    </ThemeProvider>
+    <ReduxProvider store={store}>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider theme={theme}>
+          <ApplicationRoutes />
+          <GlobalStyles />
+        </ThemeProvider>
+      </QueryClientProvider>
+    </ReduxProvider>
   );
 };
--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import * as S from './styles';
 
 import { Button, Heading, Modal, TextField } from '@/components';
 
+import { login } from '@/redux/reducers/authReducer';
+
 export const SignIn = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const [username, setUsername] = useState('');
+
+  function handleNavigation() {
+    dispatch(login(username));
+    navigate('/posts');
+  }
+
   return (
     <Modal isOpen={true} size="small">
       <Heading>Welcome to CodeLeap network!</Heading>
       <S.InputGroup>
-        <TextField label="Please enter your username" placeholder="Jhon doe" />
+        <TextField
+          label="Please enter your username (required)"
+          placeholder="Jhon doe"
+          value={username}
+          onChange={value => setUsername(value.target.value)}
+          autoFocus
+          aria-required
+        />
       </S.InputGroup>
       <S.ButtonGroup>
-        <Button>ENTER</Button>
+        <Button onClick={() => handleNavigation()} isDisabled={!username}>
+          ENTER
+        </Button>
       </S.ButtonGroup>
     </Modal>
   );

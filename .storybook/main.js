59a41459888eca300e55c3fcc8c6d4751b30007b
hleao <henrique.leao@softvaro.com.br>
2022-09-10 15:40:52 -0300

    feat: :sparkles: added @svg/webpack to storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
@@ -10,7 +12,24 @@ module.exports = {
     builder: '@storybook/builder-webpack5'
   },
   webpackFinal: config => {
-    config.resolve.modules.push(`${process.cwd()}/src`);
-    return config;
+    config.resolve.plugins = [
+      ...(config.resolve.plugins || []),
+      new TsconfigPathsPlugin({
+        extensions: config.resolve.extensions
+      })
+    ];
+
+    const fileLoaderRule = config.module.rules.find(
+      rule => !Array.isArray(rule.test) && rule.test.test('.svg')
+    );
+    fileLoaderRule.exclude = /\.svg$/;
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack']
+    });
+
+    return {
+      ...config
+    };
   }
 };
